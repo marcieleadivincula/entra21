985c836467a2b0ca572f3c01aa1c39080c219184
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-08-24 15:57:44 -0300

    Aula de Struct

---
+++ StructExercicios/Struct-estrutura/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Struct_estrutura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declara um objeto da struct criada
+            Pessoa pessoa = new Pessoa();
+            pessoa.nome = "Julieta";
+            pessoa.idade = 57;
+
+            Console.WriteLine(pessoa.nome + "\n" + pessoa.idade);
+
+            //Declara um vetor de objetos da struct criada
+            Pessoa[]  vetPessoa = new Pessoa[5];
+            for (int i = 0; i < vetPessoa.Length; i++)
+            {
+                vetPessoa[i].nome = "ABC |" + i;
+                vetPessoa[i].idade = 33 + i * 4;
+                Console.WriteLine(vetPessoa[i].nome + "\n" + vetPessoa[i].idade + "\n --------------");
+            }
+
+        }
+
+        //Encapsulamento de informação
+        public struct Pessoa {
+            public string nome;
+            public int idade;
+            public bool vivo;
+            public double salario;
+        }
+        public class Person
+        {
+            public string nome;
+            public int idade;
+            public bool vivo;
+            public double salario;
+        }
+    }
+}
