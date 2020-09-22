f74f32fad719af6c1d95ab125ccd7559b8a641da
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-09-22 16:26:50 -0300

    OOpt102
    Atualizando lista OOpt102

--- OrientacaoObjeto/ExerciciosOOpt102/Program.cs
+++ OrientacaoObjeto/ExerciciosOOpt102/Program.cs
@@ -6,142 +6,40 @@ namespace ExerciciosOOpt102
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Exercícios de Orientação à Objeto - 2");
+            Console.WriteLine("Exercícios de Orientação à Objeto - 1.02");
 
-            //1
-            //Cliente[] cliente = new Cliente[4];
+            //1 Crie 4 clientes com nome,cpf e VIP. Peça para o usuario preencher os clientes, em seguida, mostre todos os clientes começando pelos VIP’s e com um selo(qualque símbolo serve).
+            Cliente[] clientes = new Cliente[4];
 
-            //for (int i = 0; i < cliente.Length; i++)
-            //{
-            //    Console.WriteLine("Insira o nome, CPF e VIP do {0}° cliente", (i + 1));
-            //    cliente[i] = new Cliente();
-
-            //    Console.Write("Digite o nome do cliente: ");
-            //    cliente[i].nome = Console.In.ReadLine();
-            //    Console.Write("Digite o CPF do cliente: ");
-            //    cliente[i].cpf = Console.In.ReadLine();
-            //    Console.Write("Digite v para vip ou n para não: ");
-            //    cliente[i].vip = Convert.ToChar(Console.In.ReadLine());
-            //}
-
-            //if (cliente[0].vip == 'v')
-            //{
-            //    cliente[0].nome = cliente[0].nome + "#_#";
-            //}
-
-            //for (int i = 0; i < cliente.Length; i++)
-            //{
-            //    for (int j = i; j > 0; j--)
-            //    {
-            //        if (cliente[j - 1].vip < cliente[j].vip)
-            //        {
-            //            cliente[j].nome = cliente[j].nome + "#_#";
-            //            Cliente temp = new Cliente();
-            //            cliente[j] = cliente[j - 1];
-            //            cliente[j - 1] = temp;
-            //        }
-            //        else
-            //        {
-            //            break;
-            //        }
-            //    }
-            //}
-
-            //for (int i = 0; i < cliente.Length; i++)
-            //{
-            //    if (cliente[i].vip == 'v')
-            //    {
-            //        Console.WriteLine("{0} {1}", cliente[i].nome + "#_#", cliente[i].cpf);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("{0} {1}", cliente[i].nome, cliente[i].cpf);
-            //    }
-            //}
-
-            //2
-            //Estacionamento[] estacionamento = new Estacionamento[5];
-
-            //for (int i = 0; i < estacionamento.Length; i++)
-            //{
-            //    Console.WriteLine("Insira o nome do dono e a placa do {0}° carro", (i+1));
-            //    estacionamento[i] = new Estacionamento();
-
-            //    Console.Write("Nome do dono: ");
-            //    estacionamento[i].nomeDono = Console.In.ReadLine();
-            //    Console.Write("Placa do carro: ");
-            //    estacionamento[i].placaCarro = Console.In.ReadLine();
-            //}
-
-            //for (int i = 0; i < estacionamento.Length; i++)
-            //{
-            //    if (estacionamento[i].nomeDono != "")
-            //    {
-            //        Console.WriteLine("{0} {1}", estacionamento[i].nomeDono, estacionamento[i].placaCarro);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("{0} {1}", estacionamento[i].nomeDono ,estacionamento[i].placaCarro);
-            //    }
-            //}
-
-            //3
-            //Hotel[] hotel = new Hotel[5];
-
-            //for (int i = 0; i < hotel.Length; i++)
-            //{
-            //    hotel[i] = new Hotel();
-            //    Console.WriteLine("Insira o nome e o número de quartos: ");
-
-            //    Console.Write("Nome: ");
-            //    hotel[i].nome = Console.In.ReadLine();
-            //    Console.Write("Quantidade de quartos de solteiro: ");
-            //    hotel[i].qtdSolteiro = Convert.ToInt32(Console.In.ReadLine());
-            //    Console.Write("Quantidade de quartos de casal: ");
-            //    hotel[i].qtdCasal = Convert.ToInt32(Console.In.ReadLine());
-            //}
-
-            //for (int i = 0; i < hotel.Length; i++)
-            //{
-            //    for (int j = i; j > 0; j--)
-            //    {
-            //        if ((hotel[j-1].qtdCasal * 2) + (hotel[j - 1].qtdSolteiro) < (hotel[j].qtdCasal * 2) + (hotel[j].qtdSolteiro))
-            //        {
-            //            Hotel ht = hotel[j];
-            //            hotel[j - 1] = hotel[j-1];
-            //            hotel[j - 1] = ht;
-            //        }
-            //    }
-            //}
-
-            //for (int i = 0; i < hotel.Length; i++)
-            //{
-            //    Console.WriteLine("{0} {1} {2}", hotel[i].nome, hotel[i].qtdSolteiro, hotel[i].qtdCasal);
-            //}
-
-            //4
-            Pessoa[] pes = new Pessoa[10];
-
-            for (int i = 0; i < pes.Length; i++)
+            for (int i = 0; i < clientes.Length; i++)
             {
-                Console.WriteLine("Insira a {0}° pessoa", (i + 1));
-                pes[i] = new Pessoa();
-                Console.Write("Nome: ");
-                pes[i].nome = Console.In.ReadLine();
-                Console.Write("Idade: ");
-                pes[i].idade = Convert.ToInt32(Console.In.ReadLine());
+                Console.WriteLine("Insira o nome, CPF e VIP do {0}° cliente", (i + 1));
+
+                Console.Write("Digite o nome do cliente: ");
+                string nome = Console.In.ReadLine();
+                Console.Write("Digite o CPF do cliente: ");
+                string cpf = Console.In.ReadLine();
+                Console.Write("Digite v para vip ou n para não: ");
+                char vip = Convert.ToChar(Console.In.ReadLine());
+
+                clientes[i] = new Cliente(nome, cpf, vip);
             }
 
-            //&& EhAdult() == "true"
-            for (int i = 0; i < pes.Length; i++)
+            if (clientes[0].GetVip() == 'v')
+            {
+                clientes[0].GetNome() = clientes[0].GetNome() + "#_#";
+            }
+
+            for (int i = 0; i < clientes.Length; i++)
             {
                 for (int j = i; j > 0; j--)
                 {
-                    if (pes[j - 1].idade < pes[j].idade)
+                    if (clientes[j - 1].GetVip() < clientes[j].GetVip())
                     {
-                        Pessoa temp = pes[j - 1];
-                        pes[j - 1] = pes[j];
-                        pes[j] = temp;
+                        clientes[j].GetNome() = clientes[j].GetNome() + "#_#";
+                        Cliente temp = new Cliente();
+                        clientes[j] = clientes[j - 1];
+                        clientes[j - 1] = temp;
                     }
                     else
                     {
@@ -150,13 +48,123 @@ namespace ExerciciosOOpt102
                 }
             }
 
-            for (int i = 0; i < pes.Length; i++)
+            for (int i = 0; i < clientes.Length; i++)
             {
-                if (pes[i].EhAdult() == true)
+                if (clientes[i].GetVip() == 'v')
                 {
-                    Console.WriteLine("{0} {1}", pes[i].nome, pes[i].idade);
+                    Console.WriteLine("{0} {1}", clientes[i].GetNome() + "#_#", clientes[i].GetCpf());
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1}", clientes[i].GetNome(), clientes[i].GetCpf());
                 }
             }
+
+            //2 ) Crie 5 vagas de estacionamento com placa do veículo e nome do dono. Peça para o usuário preencher as vagas(pode-se deixar vazio), em seguida, mostre todas as vagas preenchidas.
+            //Estacionamento[] estacionamento = new Estacionamento[5];
+
+            //for (int i = 0; i < estacionamento.Length; i++)
+            //{
+            //    Console.WriteLine("Insira o nome do dono e a placa do {0}° carro", (i + 1));
+
+            //    Console.Write("Nome do dono: ");
+            //    string nomeDono = Console.In.ReadLine();
+            //    Console.Write("Placa do carro: ");
+            //    string placaCarro = Console.In.ReadLine();
+
+            //    estacionamento[i] = new Estacionamento(nomeDono, placaCarro);
+            //}
+
+            //for (int i = 0; i < estacionamento.Length; i++)
+            //{
+            //    if (estacionamento[i].GetNomeDono() != "")
+            //    {
+            //        Console.WriteLine("{0} {1}", estacionamento[i].GetNomeDono(), estacionamento[i].GetPlacaCarro());
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("{0} {1}", estacionamento[i].GetNomeDono(), estacionamento[i].GetPlacaCarro());
+            //    }
+            //}
+
+            //3 Crie 5 hotéis com nome,quantidade de quartos de solteiro e quantidade de quartos de casal e peça para o usuário preencher os hotéis, após isso, mostre na tela do hotel com mais vaga disponível para o com menos vaga disponível
+            //OBS: Considere que os hotéis estão vazios, Cada quarto de casal representa duas vagas e o quarto de solteiro uma vaga.
+
+            //Hotel[] hotel = new Hotel[5];
+
+            //for (int i = 0; i < hotel.Length; i++)
+            //{
+            //    Console.WriteLine("Insira o nome e o número de quartos: ");
+
+            //    Console.Write("Nome: ");
+            //   string nome = Console.In.ReadLine();
+            //    Console.Write("Quantidade de quartos de solteiro: ");
+            //   int qtdSolteiro = Convert.ToInt32(Console.In.ReadLine());
+            //    Console.Write("Quantidade de quartos de casal: ");
+            //   int qtdCasal = Convert.ToInt32(Console.In.ReadLine());
+
+            //    hotel[i] = new Hotel(nome, qtdSolteiro, qtdCasal);
+            //}
+
+            //for (int i = 0; i < hotel.Length; i++)
+            //{
+            //    for (int j = i; j > 0; j--)
+            //    {
+            //        if ((hotel[j - 1].MultiplicaQtdCasal()) + (hotel[j - 1].GetQtdSolteiro()) < (hotel[j].MultiplicaQtdCasal()) + (hotel[j].GetQtdSolteiro()))
+            //        {
+            //            Hotel ht = hotel[j];
+            //            hotel[j - 1] = hotel[j - 1];
+            //            hotel[j - 1] = ht;
+            //        }
+            //    }
+            //}
+
+            //for (int i = 0; i < hotel.Length; i++)
+            //{
+            //    Console.WriteLine("{0} {1} {2}", hotel[i].GetNome(), hotel[i].GetQtdSolteiro(), hotel[i].GetQtdCasal());
+            //}
+
+            //4 Crie 10 pessoas com nome,idade e uma função que retorna verdadeiro caso a pessoa seja adulta. Peça para o usuário preencher todas as pessoas, em seguida, mostre em ordem do mais velho para o mais novo apenas aqueles que são adultos
+            //ps: deve - se usar o retorno da função para avalaiar se é adulto ou não
+            //Pessoa[] pes = new Pessoa[10];
+
+            //for (int i = 0; i < pes.Length; i++)
+            //{
+            //    Console.WriteLine("Insira a {0}° pessoa", (i + 1));
+            //    Console.Write("Nome: ");
+            //    string nome = Console.In.ReadLine();
+            //    Console.Write("Idade: ");
+            //    int idade = Convert.ToInt32(Console.In.ReadLine());
+
+            //    pes[i] = new Pessoa(nome, idade);
+            //    Console.WriteLine();
+            //}
+
+            ////&& EhAdult() == "true"
+            //for (int i = 0; i < pes.Length; i++)
+            //{
+            //    for (int j = i; j > 0; j--)
+            //    {
+            //        if (pes[j - 1].GetIdade() < pes[j].GetIdade())
+            //        {
+            //            Pessoa temp = pes[j - 1];
+            //            pes[j - 1] = pes[j];
+            //            pes[j] = temp;
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //for (int i = 0; i < pes.Length; i++)
+            //{
+            //    if (pes[i].EhAdult() == true)
+            //    {
+            //        Console.WriteLine("Nome: {0}, Idade: {1}", pes[i].GetNome(), pes[i].GetIdade());
+            //    }
+            //}
         }
     }
 }
