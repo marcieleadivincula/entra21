e4055442318af473e1f38fe38cb36c3a75389725
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-09-22 16:24:24 -0300

    Class Cliente
    Criando atributos, construtor e métodos da classe

--- OrientacaoObjeto/ExerciciosOOpt102/Cliente.cs
+++ OrientacaoObjeto/ExerciciosOOpt102/Cliente.cs
@@ -6,8 +6,50 @@ namespace ExerciciosOOpt102
 {
     class Cliente
     {
-        public string nome;
-        public string cpf;
-        public char vip;
+        private string _nome;
+        private string _cpf;
+        private char _vip;
+
+        public Cliente()
+        {
+    
+        }
+
+        public Cliente(string nome, string cpf, char vip)
+        {
+            this._nome = nome;
+            this._cpf = cpf;
+            this._vip = vip;
+        }
+
+        public void SetNome(string nome)
+        {
+            this._nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return this._nome;
+        }
+
+        public void SetCpf(string cpf)
+        {
+            this._cpf = cpf;
+        }
+
+        public string GetCpf()
+        {
+            return this._cpf;
+        }
+
+        public void SetVip(char vip)
+        {
+            this._vip = vip;
+        }
+
+        public char GetVip()
+        {
+            return this._vip;
+        }
     }
 }
