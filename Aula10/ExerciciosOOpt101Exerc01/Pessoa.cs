609e80d36d75c3e739b455f7398996293fec3953
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-09-24 16:26:00 -0300

    Refazendo exercicios 01, 02  e 03

---
+++ Aula10/ExerciciosOOpt101Exerc01/Pessoa.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosOOpt101Exerc01
+{
+    class Pessoa
+    {
+        private string _nome;
+        private string _cpf;
+        private int _idade;
+        private Endereco _endereco;
+
+        public Pessoa(string nome, string cpf, int idade, Endereco endereco)
+        {
+            this._nome = nome;
+            this._cpf = cpf;
+            this._idade = idade;
+            this._endereco = endereco;
+        }
+
+        public void SetNome(string nome)
+        {
+            this._nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return this._nome;
+        }
+
+        public void SetCpf(string cpf)
+        {
+            this._cpf = cpf;
+        }
+
+        public string GetCpf()
+        {
+            return this._cpf;
+        }
+
+        public void SetIdade(int idade)
+        {
+            this._idade = idade;
+        }
+
+        public int GetIdade()
+        {
+            return this._idade;
+        }
+
+        public void SetEndereco(Endereco endereco)
+        {
+            this._endereco = endereco;
+        }
+
+        public Endereco GetEndereco()
+        {
+            return this._endereco;
+        }
+    }
+}
