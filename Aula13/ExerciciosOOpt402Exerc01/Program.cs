bb3c79138e859b0ed5666b0d5e819361969239e6
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-06 15:10:35 -0300

    Criando lista 402 Exercicio 01

---
+++ Aula13/ExerciciosOOpt402Exerc01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosOOpt402Exerc01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
