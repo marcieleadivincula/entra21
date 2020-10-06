0e649bc28f3a8f13f6217873c987bc8250d7039c
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-06 16:29:09 -0300

    Iniciando exercicio 02 da lista OOpt103

---
+++ Aula10/ExerciciosOOpt103Exerc02/Produto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosOOpt103Exerc02
+{
+    class Produto
+    {
+        private string _nomeProduto;
+        private int _idProduto;
+        private double _precoProduto;
+
+        public Produto(string nomeProduto, int idProduto, double precoProduto)
+        {
+            _nomeProduto = nomeProduto;
+            _idProduto = idProduto;
+            _precoProduto = precoProduto;
+        }
+
+        public void SetNomeProduto(string nomeProduto)
+        {
+            this._nomeProduto = nomeProduto;
+        }
+
+        public string GetNomeProduto()
+        {
+            return this._nomeProduto;
+        }
+
+        public void SetIdProduto(int idProduto)
+        {
+            this._idProduto = idProduto;
+        }
+
+        public int GetIdProduto()
+        {
+            return this._idProduto;
+        }
+
+        public void SetPrecoProduto(double precoProduto)
+        {
+            this._precoProduto = precoProduto;
+        }
+
+        public double GetPrecoProduto()
+        {
+            return this._precoProduto;
+        }
+    }
+}
--- Aula10/ExerciciosOOpt103Exerc02/Program.cs
+++ Aula10/ExerciciosOOpt103Exerc02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ExerciciosOOpt103Exerc02
 {
@@ -9,6 +10,38 @@ namespace ExerciciosOOpt103Exerc02
             Console.WriteLine("Exercícios de Orientação à Objeto - 3");
 
             //2) Crie 5 produtos para um mercado com nome do produto, id e preço. Peça para o usuário preencher todos os produtos(nao pode ter produtos com o mesmo id), em seguida, crie um carrinho de compras virtual que PODE ter até 20 produtos , ao finalizar a compra deve-se dizer ao usuario o nome de todos os produtos comprados e some o valor final.
+
+            Produto[] pro = new Produto[2];
+            ArrayList listaProdutos = new ArrayList();
+
+            for (int i = 0; i < pro.Length; i++)
+            {
+                Console.WriteLine("Insira a {0}° produto", (i + 1));
+
+                Console.Write("Nome do produto: ");
+                string nome = Console.In.ReadLine();
+                Console.Write("Id do produto: ");
+                int id = int.Parse(Console.In.ReadLine());
+                Console.Write("Preço do produto: ");
+                double preco = double.Parse(Console.In.ReadLine());
+
+                pro[i] = new Produto(nome, id, preco);
+
+                listaProdutos = new ArrayList();
+
+                listaProdutos.Add(pro[i]);
+            }
+
+            foreach (Produto produto in listaProdutos)
+            {
+                Console.WriteLine("Count: ", listaProdutos.Count);
+                Console.WriteLine("Contains: ", listaProdutos.Contains(produto));
+            }
+
+            for (int i = 0; i < pro.Length; i++)
+            {
+                Console.WriteLine("Nome do produto: {0} Id: {1} Preço: {2}", pro[i].GetNomeProduto(), pro[i].GetIdProduto(), pro[i].GetPrecoProduto());
+            }
         }
     }
 }
