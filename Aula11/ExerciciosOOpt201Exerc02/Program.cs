3fd292f4f96553d5dd1cd8c29957306d6568400c
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-20 16:28:21 -0300

    Criando classe Pessoa

--- Aula11/ExerciciosOOpt201Exerc02/Program.cs
+++ Aula11/ExerciciosOOpt201Exerc02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ExerciciosOOpt201Exerc02
 {
@@ -8,6 +9,38 @@ namespace ExerciciosOOpt201Exerc02
         {
             Console.WriteLine("Lista de Exercícios - OOpt201");
             //2) Crie 4 pessoas com nome,idade e genero. Peça para o usuario preencher as pessoas, em seguida, diga qual o homem mais velho e qual a mulher mais velha.
+            Pessoa[] pes = new Pessoa[4];
+            int idade;
+
+            for (int i = 0; i < pes.Length; i++)
+            {
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Idade: ");
+                idade = int.Parse(Console.ReadLine());
+                Console.Write("Gênero: ");
+                char genero = char.Parse(Console.ReadLine());
+
+                pes[i] = new Pessoa(nome, idade, genero);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < pes.Length; i++)
+            {
+                if (pes[i].ValidaGenero() == true)
+                {
+                    Console.WriteLine("Nome: {0} Idade: {1} Gênero: {2}", pes[i].Nome, pes[i].Idade, pes[i].Genero);
+                }
+                else if (pes[i].ValidaGenero() == false)
+                {
+                    Console.WriteLine("Nome: {0} Idade: {1} Gênero: {2}", pes[i].Nome, pes[i].Idade, pes[i].Genero);
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido!");
+                    break;
+                }
+            }
         }
     }
 }
