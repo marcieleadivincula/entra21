9b6a1e6ffb2cbb2ee3f0eaa2f51415ffa2a8f8fa
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-20 16:28:43 -0300

    Criando classe Pessoa

---
+++ Aula11/ExerciciosOOpt201Exerc02/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExerciciosOOpt201Exerc02
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Genero { get; set; }
+
+        public Pessoa(string nome, int idade, char genero)
+        {
+            Nome = nome;
+            Idade = idade;
+            Genero = genero;
+        }
+        public bool ValidaGenero()
+        {
+            if (Genero == 'f')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
