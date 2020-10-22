6a29b2568b0d34987f450ad47e05e9e1af5b7deb
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-22 15:32:49 -0300

    Iniciando exercicio1 da lista 601

---
+++ Aula15/ExerciciosDeOOpt0601Exerc01/Vip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosDeOOpt0601Exerc01
+{
+    class Vip : Ingresso
+    {
+        public double ValorAdicional { get; set; }
+
+        //Sobreencre - va um método que retorne o valor do ingresso VIP(com o adicional incluído).
+        public override double ImprimeValor()
+        {
+            double adicional = ValorEmReais + ValorAdicional;
+            return adicional;
+        }
+    }
+}
