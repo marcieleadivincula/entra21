ae69d148711166130c4b942d73d550e0d40ea874
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-27 15:22:46 -0300

    Criando classe Endereco

---
+++ Aula14/ExercíciosDeOOpt0501Exerc02/Endereco.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercíciosDeOOpt0501Exerc02
+{
+    class Endereco
+    {
+        //rua, cep, cidade e estado
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        public Endereco(string rua, string numero, string cep, string bairro, string cidade, string estado)
+        {
+            Rua = rua;
+            Numero = numero;
+            Cep = cep;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+        }
+    }
+}
