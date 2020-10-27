d6d8b10d195586ec357b1d8277dac4780d709a12
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-27 15:22:58 -0300

    Criando classe Funcionario

---
+++ Aula14/ExercíciosDeOOpt0501Exerc02/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercíciosDeOOpt0501Exerc02
+{
+    class Funcionario
+    {
+        //herda Endereco e herda Pessoa
+        public string Cargo { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string cargo, double salario)
+        {
+            Cargo = cargo;
+            Salario = salario;
+        }
+    }
+}
