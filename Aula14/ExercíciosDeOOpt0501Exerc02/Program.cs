76211ee846927b3b851263f0c47061b952b50fcf
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-27 15:24:14 -0300

    Iniciando lista de funcionarios

--- Aula14/ExercíciosDeOOpt0501Exerc02/Program.cs
+++ Aula14/ExercíciosDeOOpt0501Exerc02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ExercíciosDeOOpt0501Exerc02
 {
@@ -12,6 +13,8 @@ namespace ExercíciosDeOOpt0501Exerc02
             //a) Crie uma subClasse chamado Funcionario com cargo, salario, herda Endereco e herda Pessoa.Crie e preencha 3 Funcionarios, após isso, mostre na tela os funcionarios que possuem um salario igual ou maior que 1800, caso tenha 1 ou mais funcionarios com salario inferior a 1800, mostre apenas aqueles que tem salário inferior a 1800.
 
             //b) Crie uma subClasse chamado Mae com nome do cônjuge(se houver), quantidade de filhos(se houver), herda Endereco e herda Pessoa.Crie e preencha uma Mae, após isso, mostre na tela e pergunte se deseja alterar alguma coisa e oque, caso nao queira, encerre.
+            List<Funcionario> func = new List<Funcionario>();
+
 
         }
     }
