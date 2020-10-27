7e81a5d39f9c9adf54823422eae11b9dbbdb9f6f
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-27 15:23:05 -0300

    Criando classe Mae

---
+++ Aula14/ExercíciosDeOOpt0501Exerc02/Mae.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercíciosDeOOpt0501Exerc02
+{
+    class Mae
+    {
+        //herda Endereco e herda Pessoa
+        public string NomeConjuge { get; set; }
+        public int QtdFilhos { get; set; }
+
+        public Mae(string nomeConjuge, int qtdFilhos)
+        {
+            NomeConjuge = nomeConjuge;
+            QtdFilhos = qtdFilhos;
+        }
+    }
+}
