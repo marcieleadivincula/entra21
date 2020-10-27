e3e1a1e3005c6a2321006eb982ae02e953a5e904
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-27 15:23:12 -0300

    Criando classe Pessoa

---
+++ Aula14/ExercíciosDeOOpt0501Exerc02/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercíciosDeOOpt0501Exerc02
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string CPF { get; set; }
+
+        public Pessoa(string nome, int idade, string cpf)
+        {
+            Nome = nome;
+            Idade = idade;
+            CPF = cpf;
+        }
+    }
+}
