dd46f71f8a3caf39363b57186d543954dd7d9276
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:36:57 -0300

    Criando class Normal

---
+++ Aula15/ExerciciosDeOOpt0601Exerc01/Normal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosDeOOpt0601Exerc01
+{
+    class Normal : Ingresso
+    {
+        //b) crie uma classe Normal, que herda Ingresso e possui um método que imprime: "Ingresso Normal"
+        public double ValorAdicional { get; set; }
+        public override double ImprimeValor()
+        {
+            double adicional = ValorEmReais + ValorAdicional;
+            return adicional;
+        }
+    }
+}
