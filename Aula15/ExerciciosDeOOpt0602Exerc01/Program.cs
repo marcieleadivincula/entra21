1325f4c5b0cd01848b3015add448cfb5c3b875a8
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 16:17:29 -0300

    Exercicio 01 da lista 602

--- Aula15/ExerciciosDeOOpt0602Exerc01/Program.cs
+++ Aula15/ExerciciosDeOOpt0602Exerc01/Program.cs
@@ -7,13 +7,15 @@ namespace ExerciciosDeOOpt0602Exerc01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //1) Crie uma agenda de contatos. Nessa agenda, contatos podem ser adicionados, removidos, ou pesquisados por nome ou por CPF/CNPJ. Deve ser possível, tam-bém, visualizar todos os contatos da agenda.
+            //1) Crie uma agenda de contatos. Nessa agenda, contatos podem ser adicionados, removidos, ou pesquisados por nome ou por CPF/CNPJ. Deve ser possível, também, visualizar todos os contatos da agenda.
 
-            //a) Dois tipos de contatos podem fazer parte da agenda: pessoa física e pessoa jurí - dica.A pessao física tem os seguintes atributos: CPF, nome, endereço, data de nas - cimento, email, estado civil, outros que você achar necessários. A pessoa jurídica possui: CNPJ, nome, endereço, email, inscrição estadual, razão social, outros que vc achar necessários.
+            //a) Dois tipos de contatos podem fazer parte da agenda: pessoa física e pessoa jurídica.A pessao física tem os seguintes atributos: CPF, nome, endereço, data de nas - cimento, email, estado civil, outros que você achar necessários. A pessoa jurídica possui: CNPJ, nome, endereço, email, inscrição estadual, razão social, outros que vc achar necessários.
 
             //b) Na classe agenda acima, implemente um método ordena que coloca os objetos em ordem crescente de CPF/ CNPJ.Além disso, todas as pessoas físicas devem aparecer antes das pessoas jurídicas.Note que o seu programa deve fazer a ordenação, im - plementando algum dos algoritmos conhecidos
 
             //Dica: o Método ordena pode receber obsjetos como parâmetro
+
+
 
         }
     }
