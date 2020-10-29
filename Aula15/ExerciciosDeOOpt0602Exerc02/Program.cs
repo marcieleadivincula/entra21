18f27da5a81bf4ec8d393a74b5332d808757ddfb
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:39:24 -0300

    Iniciando exercicio02 da lista 602

---
+++ Aula15/ExerciciosDeOOpt0602Exerc02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExerciciosDeOOpt0602Exerc02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //2) Crie uma Classe Pai chamado Produto com nome, preco, tipo e CalculaPreco().
+
+            //a) Crie uma SubClasse chamado Mouse com dpi, modelo e herda Produto, além dis - so, ele sobreescreve o método CalculaPreco()(o preco é calculado da seguinte forma, preco = (preco * 2.3) + dpi)
+
+            //b) Crie uma SubClasse chamado Livro com autor, estilo e herda Produto, além disso, ele sobreescreve o método CalculaPreco()(o preco é calculado da seguinte forma, preco = preco * 1.55)
+
+            //c) Crie 3 mouses e livros diferentes, então mostre todos para o usuario, e pergunte quais ele deseja adicionar em seu carrinho, após isso, mostre quanto o carrinho esta custando
+
+        }
+    }
+}
