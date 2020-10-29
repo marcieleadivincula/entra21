99818d18efec1c24e9c2bb0c368275998a5fdea4
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:41:31 -0300

    Iniciando exercicio02 da lista 102

---
+++ Aula16/ExerciciosDeVAOpt102Exerc02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExerciciosDeVAOpt102Exerc02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //2) Crie uma Classe para representar um Banco de Dados contendo id, nome,cpf,cargo. Começe criando uma lista desse BD, após isso, peça para o usuário preencher um elemento. Ao terminar de preen-cher,peça se deseja adicionar mais um elemento, caso nao, mostre tudo para o usuario e pergunte se de-seja alterar algo, se SIM , pergunte quem ele deseja alterar e peça todos os dados, se NAO, termine o programa (ps : o usuario pode editar quantas informações ele desejar)
+            //OBS: o id SEMPRE incrementa em 1 para cada registro ,começando em 0
+
+        }
+    }
+}
