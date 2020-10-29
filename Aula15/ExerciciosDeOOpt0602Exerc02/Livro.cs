589f8b63d767b65ed42e321f65b708206223e772
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 16:17:20 -0300

    Exercicio 02 da lista 602

---
+++ Aula15/ExerciciosDeOOpt0602Exerc02/Livro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosDeOOpt0602Exerc02
+{
+    class Livro : Produto
+    {
+        //b) Crie uma SubClasse chamado Livro com autor, estilo e herda Produto, além disso, ele sobreescreve o método CalculaPreco()(o preco é calculado da seguinte forma, preco = preco * 1.55)
+
+        public string Autor { get; set; }
+        public string Estilo { get; set; }
+
+        public override double CalculaPreco()
+        {
+            return Preco *= 1.55;
+        }
+
+        public Livro(string autor, string estilo, string nome, double preco, string tipo): base(nome, preco, tipo)
+        {
+            Autor = autor;
+            Estilo = estilo;
+        }
+    }
+}
--- Aula15/ExerciciosDeOOpt0602Exerc02/Program.cs
+++ Aula15/ExerciciosDeOOpt0602Exerc02/Program.cs
@@ -14,7 +14,11 @@ namespace ExerciciosDeOOpt0602Exerc02
             //b) Crie uma SubClasse chamado Livro com autor, estilo e herda Produto, além disso, ele sobreescreve o método CalculaPreco()(o preco é calculado da seguinte forma, preco = preco * 1.55)
 
             //c) Crie 3 mouses e livros diferentes, então mostre todos para o usuario, e pergunte quais ele deseja adicionar em seu carrinho, após isso, mostre quanto o carrinho esta custando
+            
+            //Mouse mouse = new Mouse();
+            //Livro livro = new Livro();
 
+            //Console.WriteLine(mouse.Nome, mouse.Preco, mouse.Tipo, mouse.DPI);
         }
     }
 }
