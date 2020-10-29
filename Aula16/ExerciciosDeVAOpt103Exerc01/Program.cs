6e7a21c683f521d758e447730bb37ba7f9208ac4
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:41:50 -0300

    Iniciando exercicio01 da lista 103

---
+++ Aula16/ExerciciosDeVAOpt103Exerc01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExerciciosDeVAOpt103Exerc01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //1) Crie uma classe chamado Conta com numeroConta e valorNaConta.
+
+            //a) Crie um Caixa eletronico que possui Saldo ,função Saque(Conta), e função Deposito(Conta) ambas recebem Conta como parâmetro.
+
+            //b) Crie agora uma Lista com 5 Contas(inicialmente), antes de efetuar acesso em uma conta, pode - se adicionar contas extras.
+
+            //c) Vamos agora criar o Menu, para ter acesso ao menu, você deve primeiro pedir a conta que o usuario esta usando, se a conta existir, vai pro menu, caso nao, pede novamente, além disso, o usuario pode en-cerrar o programa em vez de tentar fazer o acesso, vamos ao Menu:
+            //1 – Sacar: Saca uma quantia, para isso, peça a quantia e confere se a Conta e o Caixa possuem saldo pra isso, caso haja, ambos perdem dinheiro equivalente a quantia, caso um nao possua, diga que o saque nao foi efetuado.
+            //2 – Deposito: Deposita uma quantia, para isso, peça ao usuario a quantia e simplesmente adiciona esse valor na Conta e no Caixa
+            //3 – Extrato: mostre quanto tem na conta
+            //4 – Encerrar: volte ao Menu anterior
+            //OBS: Sempre que uma operação for efetuada(falhando ou nao) volte ao menu
+
+        }
+    }
+}
