69bee2e337a033da83238d80fd0e660b4c9324c7
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:37:39 -0300

    Criando class Vip

--- Aula15/ExerciciosDeOOpt0601Exerc01/Vip.cs
+++ Aula15/ExerciciosDeOOpt0601Exerc01/Vip.cs
@@ -6,6 +6,7 @@ namespace ExerciciosDeOOpt0601Exerc01
 {
     class Vip : Ingresso
     {
+        //a) crie uma classe VIP, que herda Ingresso e possui um valor adicional.Sobreencreva um método que retorne o valor do ingresso VIP(com o adicional incluído).
         public double ValorAdicional { get; set; }
 
         //Sobreencre - va um método que retorne o valor do ingresso VIP(com o adicional incluído).
