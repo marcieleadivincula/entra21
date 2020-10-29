8abab6ea00618955274530cc3b2bfb9b9971dbb2
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:42:04 -0300

    Iniciando exercicio02 da lista 103

---
+++ Aula16/ExerciciosDeVAOpt103Exerc02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExerciciosDeVAOpt103Exerc02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //2) Simule uma tabela de Jogos escolares de Futebol, para isso crie um objeto chamado Time com nome, quantidadeDePartidas ,vitorias, empates , derrotas e CalculaPontuação()*.
+
+            //a) Peça ao usuario ir inserindo times até desejar parar, porém, nao pode haver uma quantidade de times ímpar(ex: 5,7,11,3 etc...)
+
+            //b) Agora, Vamos Criar o seguinte Menu, entre cada:
+            //1 – Jogo: para um Jogo peça os 2 times que vão jogar, em seguida, determine de forma aleatória quem vence, ou se tem empate e mostre o resultado na tela.Após isso, Cada um dos 2 times deve acontecer quantidadeDePartidas ++.Para determinar o vencedor basta gerar um aleatório entre 0 e 3, caso 0 o primeiro tive vence, caso 1 o segundo time vence, caso 2 ou 3 é empate.
+
+            //2 – FinalizaTorneio: Confere a quantidade de partida dos Times, caso TODOS possuam a mesma quan-tidade, mostre os Times começando pelo que possui maior quantidade de pontos, caso ALGUM time possua uma quantidade de partida diferente, diga que alguns time nao jogaram
+
+            //* : Para calcular a pontuação, para cada vitória são 3 pontos, empate 1 e derrota 0
+
+        }
+    }
+}
