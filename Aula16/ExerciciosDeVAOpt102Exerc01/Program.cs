77a3530deb45391227db31eb15763c6cbe77bf02
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:41:14 -0300

    Iniciando exercicio01 da lista 102

---
+++ Aula16/ExerciciosDeVAOpt102Exerc01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExerciciosDeVAOpt102Exerc01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //1) Crie um objeto Chamado Produto com nome,categoria(int),preco e uma Função CalculaPreco(); ps: o preco é calculado como preco * categoria
+
+            //a) Crie 3 Produtos e peça para o usuario inserir esses produtos.
+
+            //b) Crie uma lista que representa uma cesta de compras, essa cesta pode ter N produtos, a qualquer momento o usuario pode encerrar a compra e entao, mostre o valor final da cesta.
+
+            //c) baseado no exercicio 1.b) , agora o usuario tambem pode tirar qualquer produto da cesta além das demais operações.
+
+
+        }
+    }
+}
