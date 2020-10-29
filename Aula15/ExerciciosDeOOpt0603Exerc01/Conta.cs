54a6fd1be543f9001f528957739b5d257b137a1d
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-29 14:38:32 -0300

    Criando class Conta e ContaCorrente

---
+++ Aula15/ExerciciosDeOOpt0603Exerc01/Conta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosDeOOpt0603Exerc01
+{
+    class Conta
+    {
+        public double Saldo { get; set; }
+    }
+}
---
+++ Aula15/ExerciciosDeOOpt0604Exerc01/ContaCorrente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosDeOOpt0604Exerc01
+{
+    class ContaCorrente : Conta
+    {
+        public void atualiza(double taxa)
+        {
+            this.saldo += this.saldo * taxa * 2;
+        }
+
+        public void deposita(double valor)
+        {
+            this.saldo += valor - 0.10;
+        }
+    }
+}
