f53a296788f5e39704da9b12a0b858ad407764da
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-10-30 15:10:34 -0300

    Criando construtor da classe Empresa que herda Endereco

--- Aula13/ExerciciosOOpt401Exerc03/Empresa.cs
+++ Aula13/ExerciciosOOpt401Exerc03/Empresa.cs
@@ -8,5 +8,16 @@ namespace ExerciciosOOpt401Exerc03
     {
         public string Nome { get; set; }
         public int QtdFuncionarios { get; set; }
+
+        public Empresa(string nome, int qtdFuncionarios, string rua, string numero, string bairro, string cidade, string estado) : base(rua, numero, bairro, cidade, estado)
+        {
+            Nome = nome;
+            QtdFuncionarios = qtdFuncionarios;
+        }
+
+        public Empresa()
+        {
+
+        }
     }
 }
