5fc9781e89aff83af1be257022bd2d2998d32d95
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-11-05 16:33:41 -0300

    Terminado exercicio 05 da lista 101

--- Aula17/ExercicioDeDBpt101Exerc05/Program.cs
+++ Aula17/ExercicioDeDBpt101Exerc05/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ExercicioDeDBpt101Exerc05
 {
@@ -10,6 +11,33 @@ namespace ExercicioDeDBpt101Exerc05
     {
         static void Main(string[] args)
         {
+            //5) Usando o Banco de Dados do Exercício 3, Atualize o Salário de todos os Funcionários que Possuem Menos de 3000 para exatamente 3000.
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\entra21\Desktop\marciele\entra21\Aula17\Aula17\Database1.mdf;Integrated Security=True");
+            SqlCommand cmd;
+            SqlDataReader dr;
+
+            string update = "UPDATE Funcionario SET Salario = 3000 WHERE Salario < 3000";
+            cmd = new SqlCommand(update, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            string select = "SELECT * FROM Funcionario";
+            cmd = new SqlCommand(select, conn);
+            conn.Open();
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine("Id : {0}", dr["Id"]);
+                Console.WriteLine("Nome do funcionário: {0}", dr["Nome"]);
+                Console.WriteLine("Cargo do funcionário: {0}", dr["Cargo"]);
+                Console.WriteLine("Salário do funcionário: {0}", dr["Salario"]);
+                Console.WriteLine("-----------------//------------------");
+            }
+            conn.Close();
+
+            Console.ReadLine();
         }
     }
 }
