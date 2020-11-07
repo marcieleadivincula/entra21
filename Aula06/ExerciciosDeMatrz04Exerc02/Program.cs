0bea1edd0f768dd31b1fd90b1d10c0f6f3541cae
marcieleadivincula <marcieleadivincula@gmail.com>
2020-11-07 17:04:13 -0300

    Adicionando exercicios de matriz da lista 04

---
+++ Aula06/ExerciciosDeMatrz04Exerc02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExerciciosDeMatrz04Exerc02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicios De Matriz 04");
+            //2) Faça programa que leia uma matriz 3 x 6 com valores reais.
+            //(a)Imprima a soma de todos os elementos das colunas ímpares.
+            //(b) Imprima a media aritmética dos elementos da segunda e quarta colunas.
+            //(c) Substitua os valores da sexta coluna pela soma dos valores das colunas 1 e 2.
+            //(d) Imprima a matriz modificada.
+
+        }
+    }
+}
---
+++ Aula06/ExerciciosDeMatrz04Exerc04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExerciciosDeMatrz04Exerc04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicios De Matriz 04");
+            //4) Crie o jogo da forma, para isso, crie uma matriz 3 x 3 para criar o personagem que inicia apenas com a cabeça. Quando o personagem estiver completo, o jogador perde, caso o jogador acerte todas as letras, o jogador vence.
+            //Ps: caso nao lembre como se joga o “jogo da forca” https://pt.wikipedia.org/wiki/Jogo_da_forca
+
+        }
+    }
+}
