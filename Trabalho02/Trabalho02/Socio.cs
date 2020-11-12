9a6150d59987eea778077c78b8b06b83dfc60753
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-11-12 15:03:54 -0300

    Criando classe Socio

---
+++ Trabalho02/Trabalho02/Socio.cs
@@ -0,0 +1,21 @@
+﻿namespace Trabalho02
+{
+    class Socio : Cliente
+    {
+        public double QtdAcoes { get; set; }
+
+        public Socio(double qtdAcoes, string nome, string cpf, int idade, double saldo) : base(nome, cpf, idade, saldo)
+        {
+            QtdAcoes = qtdAcoes;
+        }
+        public double GetAcoes()
+        {
+            return QtdAcoes;
+        }
+
+        public void SetAcoes(double qtdAcoes)
+        {
+            QtdAcoes = qtdAcoes;
+        }
+    }
+}
