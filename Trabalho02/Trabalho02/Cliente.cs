1db9d74b310dba9150ed910d3f99a33aa835d641
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-11-12 15:03:18 -0300

    Criando classe Cliente

---
+++ Trabalho02/Trabalho02/Cliente.cs
@@ -0,0 +1,31 @@
+﻿namespace Trabalho02
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+        public double Saldo { get; set; }
+
+        public Cliente(string nome, string cpf, int idade, double saldo)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Idade = idade;
+            Saldo = saldo;
+        }
+
+        public string GetDados()
+        {
+            return $"Nome: {Nome} CPF: {CPF} Idade: {Idade} Saldo: {Saldo}";
+        }
+
+        public void SetDados(string nome, string cpf, int idade, double saldo)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Idade = idade;
+            Saldo = saldo;
+        }
+    }
+}
