6c29f8c81eeda7ee25b1d1fe3cc0ed452fbbd023
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-11-17 15:54:13 -0300

    Update exercicios 02 03 e 04 da lista 201

--- Aula18/ExerciciosDeBDpt201Exerc03/Program.cs
+++ Aula18/ExerciciosDeBDpt201Exerc03/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace ExerciciosDeBDpt201Exerc03
 {
@@ -11,6 +13,9 @@ namespace ExerciciosDeBDpt201Exerc03
         static void Main(string[] args)
         {
             //3) Crie uma tabela pedindo pro cliente o que deseja, para isso, começe pedindo o nome da tabela e quantos elementos vai armazenar, após isso, coluna por coluna peça as seguintes informações, o nome da coluna e qual o tipo(palavra ou número)
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\entra21\Desktop\marciele\entra21\Trabalho02\Trabalho02\trabalho02.mdf;Integrated Security=True");
+            SqlCommand cmd;
+            SqlDataReader dr;
         }
     }
 }
