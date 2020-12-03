336814a3d8f3a4e2984dfa2d3553eb977ff08caa
Marciele Adivincula <marcieleadivincula@gmail.com>
2020-12-03 16:05:26 -0300

    Iniciado trabalho03

---
+++ Trabalho03/Trabalho03/GeraDados.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho03
+{
+    public static class GeraDados
+    {
+        private static Random ran = new Random();
+        public static void ColunaA()
+        {
+            int escolha = ran.Next(1, 2);
+            string letra;
+
+            switch (escolha)
+            {
+                case 1:
+                    letra = "A";
+                    break;
+                case 2:
+                    letra = "B";
+                    break;
+                default:
+                    letra = "";
+                    break;
+            }
+        }
+
+        public static void ColunaB()
+        {
+            int escolha = ran.Next(1, 2);
+            string letra;
+
+            switch (escolha)
+            {
+                case 1:
+                    letra = "A";
+                    break;
+                case 2:
+                    letra = "B";
+                    break;
+                default:
+                    letra = "";
+                    break;
+            }
+        }
+
+        public static void ColunaC()
+        {
+            int escolha = ran.Next(1, 2);
+            string letra;
+
+            switch (escolha)
+            {
+                case 1:
+                    letra = "A";
+                    break;
+                case 2:
+                    letra = "B";
+                    break;
+                default:
+                    letra = "";
+                    break;
+            }
+        }
+    }
+}
---
+++ Trabalho03/Trabalho03/Jogador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho03
+{
+    public class Jogador
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Jogador(int id, string name, int score)
+        {
+            Id = id;
+            Name = name;
+            Score = score;
+        }
+    }
+}
