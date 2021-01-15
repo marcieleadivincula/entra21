a21311df95fbae832448a6e2296f83ab6127856c
marcieleadivincula <marcieleadivincula@gmail.com>
2021-01-14 22:17:40 -0300

    Starting project Aula23

---
+++ Aula24/Aula24/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
