4c7001b1e57832eb27e7d6480f2e9f0c27936bb1
marcieleadivincula <marcieleadivincula@gmail.com>
2021-01-14 22:19:40 -0300

    Praticando com exemplos da DOC oficial

--- Aula03/Aula03/Program.cs
+++ Aula03/Aula03/Program.cs
@@ -6,7 +6,82 @@ namespace Aula03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ARRAY
+            //1- Uma Dimensão
+            //definindo o tamanho e o operador new
+            int[] numeros = new int[5] { 1, 2, 3, 4, 5 };
+            string[] nomes = new string[3] { "Mac", "Jessica", "MiMi" };
+            //omitindo o tamanho do array
+            int[] numeros2 = new int[] { 1, 2, 3, 4, 5 };
+            string[] nomes2 = new string[] { "Mac", "Jessica", "MiMi" };
+            // Omitindo o operador new
+            int[] numeros3 = { 1, 2, 3, 4, 5 };
+            string[] nomes3 = { "Mac", "Jessica", "MiMi" };
+
+            //2- Mais de uma Dimensão
+            //definindo o tamanho e o operador new
+            int[,] numeros4 = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            string[,] amigos = new string[2, 2] { { "Mac", "Jan" }, { "Mimi", "Jeff" } };
+            //omitindo o tamanho do array
+            int[,] numeros5 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            string[,] amigos1 = new string[,] { { "Mac", "Jan" }, { "Mimi", "Jeff" } };
+            //Omitindo o operador new
+            int[,] numeros6 = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            string[,] amigos2 = { { "Mac", "Jan" }, { "Mimi", "Jeff" } };
+
+            //3- Arrays de Arrays (jagged arrays)
+            //definindo o tamanho e o operador new
+            int[][] numeros7 = new int[2][] { new int[] { 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9 } };
+            //omitindo o tamanho do array
+            int[][] numeros8 = new int[][] { new int[] { 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9 } };
+            // ou
+            int[][] numeros9 = { new int[] { 2, 3, 4 }, new int[] { 5, 6, 7, 8, 9 } };
+
+
+            //Acessando e percorrendo Arrays
+            //int[] numeros10 = { 10, 9, 8, 7, 6 };
+            //listBox1.Items.Add("Primeiro elemento: " + numeros10[0]);
+            //listBox1.Items.Add("Segundo elemento: " + numeros10[1]);
+            //listBox1.Items.Add("Terceiro elemento: " + numeros10[2]);
+            //listBox1.Items.Add("Quarto elemento: " + numeros10[3]);
+            //listBox1.Items.Add("Quinto elemento: " + numeros10[4]);
+            //OU
+            //int[,] numeros2 = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 }, { 9, 10 } };
+            //listBox2.Items.Add("numeros2[0,0] = " + numeros2[0, 0]);
+            //listBox2.Items.Add("numeros2[0,1] = " + numeros2[0, 1]);
+            //listBox2.Items.Add("numeros2[1,0] = " + numeros2[1, 0]);
+            //listBox2.Items.Add("numeros2[1,1] = " + numeros2[1, 1]);
+            //listBox2.Items.Add("numeros2[2,0] = " + numeros2[2, 0]);
+            //listBox2.Items.Add("numeros2[2,1] = " + numeros2[2, 1]);
+            //listBox2.Items.Add("numeros2[3,0] = " + numeros2[3, 0]);
+            //listBox2.Items.Add("numeros2[3,1] = " + numeros2[3, 1]);
+            //listBox2.Items.Add("numeros2[4,0] = " + numeros2[4, 0]);
+            //listBox2.Items.Add("numeros2[4,1] = " + numeros2[4, 1]);
+
+            string[,] cesta = new string[3,2] { { "Maçã", "100" }, { "Pera", "200" }, { "Melão", "300" } };
+
+
+            for (int i = 0; i < cesta.Length; i++)
+            {
+                for (int j = i; j < 2; j++)
+                {
+                    Console.WriteLine(cesta[i, j]);
+                }
+            }
+
+            foreach (var fruta in cesta)
+            {
+                Console.WriteLine("Fruta: " + fruta);
+            }
+
+            int[] array1 = new int[] { 1, 3, 5, 7, 9 };
+            string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            int[] array2 = { 1, 3, 5, 7, 9 };
+            string[] weekDays2 = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            //Matrizes de tipo de valor e de tipo de referência
+            //SomeType[] array4 = new SomeType[10];
         }
     }
 }
--- Aula03/ExerciciosDeSe01Exerc04/Program.cs
+++ Aula03/ExerciciosDeSe01Exerc04/Program.cs
@@ -8,29 +8,64 @@ namespace ExerciciosDeSe01Exerc04
         {
             Console.WriteLine("ExerciciosDeSe01Exerc04");
             //4) Receba três valores, apresente na tela apenas o maior deles
-            Console.Write("Valor de A: ");
-            int a = Convert.ToInt32(Console.In.ReadLine());
-            Console.Write("Valor de B: ");
-            int b = Convert.ToInt32(Console.In.ReadLine());
-            Console.Write("Valor de C: ");
-            int c = Convert.ToInt32(Console.In.ReadLine());
 
-            if (a > b && a > c)
+            try
             {
-                Console.WriteLine(a);
+                Console.Write("Valor de A: ");
+                int a = Convert.ToInt32(Console.In.ReadLine());
+                Console.Write("Valor de B: ");
+                int b = Convert.ToInt32(Console.In.ReadLine());
+                Console.Write("Valor de C: ");
+                int c = Convert.ToInt32(Console.In.ReadLine());
+
+                if (a > b && a > c)
+                {
+                    Console.WriteLine(a);
+                }
+                else if (b > a && b > c)
+                {
+                    Console.WriteLine(b);
+                }
+                else if (c > a && c > b)
+                {
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    Console.WriteLine("São iguais!");
+                }
             }
-            else if (b > a && b > c)
+            catch (StackOverflowException soe)
             {
-                Console.WriteLine(b);
+                Console.WriteLine("Overflow. {0}", soe.Message);
             }
-            else if (c > a && c > b)
+            catch (InvalidCastException ie)
             {
-                Console.WriteLine(c);
+                Console.WriteLine("Invalid casting. {0}", ie.Message);
             }
-            else
+            catch (FormatException f)
             {
-                Console.WriteLine("São iguais!");
+                Console.WriteLine("{0} Format Exception caught.", f.Message);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex.Message);
+
+                Console.WriteLine("ToString()     : " + ex.ToString());
+                Console.WriteLine("Message        : " + ex.Message);
+                Console.WriteLine("Source         : " + ex.Source);
+                Console.WriteLine("HelpLink       : " + ex.HelpLink);
+                Console.WriteLine("TargetSite     : " + ex.TargetSite);
+                Console.WriteLine("Inner Exception: " + ex.InnerException);
+                Console.WriteLine("Stack Trace    : " + ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished.");
+            }
+
+            //int result = (a > b && a > c) ? a : (b > a && b > c) ? b : (c > a && c > b) ? c : (a == b && b == c) ? 0 : -1;
+            //Console.WriteLine(result);
         }
     }
 }
